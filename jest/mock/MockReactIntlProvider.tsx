451597b19b25fca112d993afd6c5063e72c2197c
Adam Morsi <adammor@amdocs.com>
2022-02-03 10:00:24 +0200

    add an alternative to mock react intl provider in a comment

--- jest/mock/MockReactIntlProvider.tsx
+++ jest/mock/MockReactIntlProvider.tsx
@@ -10,3 +10,19 @@ const MockReactIntlProvider: FC<MockReactIntlProviderInterface> = ({ children, l
 );
 
 export default MockReactIntlProvider;
+
+// if you would like you can override the render function of RTL as follows
+/*import React, { FC, ReactElement } from 'react';
+import { render as rtlRender } from '@testing-library/react';
+import { IntlProvider } from 'react-intl';
+
+function render(ui: ReactElement, { locale = 'en', ...renderOptions } = {}) {
+	const Wrapper: FC = ({ children }) => <IntlProvider locale={locale}>{children}</IntlProvider>;
+	return rtlRender(ui, { wrapper: Wrapper, ...renderOptions });
+}
+
+// re-export everything
+export * from '@testing-library/react';
+
+// override render method
+export { render };*/
