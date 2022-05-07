02d829dc0a6091acedeb06c1cabc2a4aa74d5b9f
DonAdam2 <adam.morsi@yahoo.com>
2022-05-07 10:49:51 +0300

    fix jest

--- babel.config.js
+++ babel.config.js
@@ -17,6 +17,7 @@ module.exports = (api) => {
       '@babel/preset-typescript',
     ],
     plugins: [
+      '@babel/plugin-transform-runtime',
       '@babel/plugin-syntax-dynamic-import',
       '@babel/plugin-proposal-class-properties',
       '@babel/plugin-proposal-object-rest-spread',
