2441b6023f238d9a4439f81b82765f400d2e224f
DonAdam2 <adam.morsi@yahoo.com>
2022-08-25 11:59:22 +0300

    update routing components

---
+++ src/ts/routing/routingComponents/RestrictedWrapper.tsx
@@ -0,0 +1,29 @@
+import { useSelector } from 'react-redux';
+//selectors
+import { getAppUserPermissionsList } from '@/ts/store/app/selectors/AppSelectors';
+//interfaces
+import { State } from '@/ts/store/rootReducer';
+import { RestrictedWrapperInterface } from '@/ts/routing/RoutingInterfaces';
+
+const RestrictedWrapper = ({
+  requiredPermissions,
+  children,
+  notPermittedComponent,
+}: RestrictedWrapperInterface) => {
+  const userPermissionsList = useSelector((state: State) => getAppUserPermissionsList(state));
+
+  if (Array.isArray(requiredPermissions)) {
+    for (let i = 0; i < requiredPermissions.length; i++) {
+      for (let j = 0; j < userPermissionsList.length; j++) {
+        if (requiredPermissions[i] === userPermissionsList[j]) return <>{children}</>;
+      }
+    }
+  }
+  if (typeof requiredPermissions === 'string') {
+    if (userPermissionsList.findIndex((permission) => permission === requiredPermissions) > -1)
+      return <>{children}</>;
+  }
+  return <>{notPermittedComponent}</>;
+};
+
+export default RestrictedWrapper;
