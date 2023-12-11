8f6a7a28d3dadcb47c42c20569abf3eaf6000232
“ <adam.morsi@yahoo.com>
2023-12-11 09:54:53 +0200

    update major packages

--- jest/interfaces/JestInterfaces.ts
+++ jest/interfaces/JestInterfaces.ts
@@ -1,9 +1,18 @@
 import { RenderOptions } from '@testing-library/react';
-import { PreloadedState } from '@reduxjs/toolkit';
 //root state
 import { RootState } from '@/ts/store/store';
 //mock store
 import setupStore from '../mocks/store';
+
+export type DeepPartial<T> = {
+  [P in keyof T]?: T[P] extends Array<infer U>
+    ? Array<DeepPartial<U>>
+    : T[P] extends ReadonlyArray<infer U>
+      ? ReadonlyArray<DeepPartial<U>>
+      : T[P] extends object
+        ? DeepPartial<T[P]>
+        : T[P];
+};
 
 /* This type interface extends the default options for render from RTL, as well
 as allows the user to specify other things such as initialState, store. */
@@ -12,7 +21,7 @@ export interface ExtendedRenderOptionsForRouter extends Omit<RenderOptions, 'que
 }
 
 export interface ExtendedRenderOptions extends ExtendedRenderOptionsForRouter {
-  preloadedState?: PreloadedState<RootState>;
+  preloadedState?: DeepPartial<RootState>;
   store?: ReturnType<typeof setupStore>;
   locale?: string;
   initialEntries?: string[];
--- jest/mocks/store/index.ts
+++ jest/mocks/store/index.ts
@@ -1,11 +1,13 @@
-import { configureStore, PreloadedState } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 //root reducer
 import { reduxSlices } from '@/ts/store/reduxSlices';
 //root state
 import { RootState } from '@/ts/store/store';
+//interfaces
+import { DeepPartial } from '@/jest/interfaces/JestInterfaces';
 
 // Create a replica of the actual store without redux dev tools
-const setupStore = (preloadedState?: PreloadedState<RootState>) =>
+const setupStore = (preloadedState?: DeepPartial<RootState>) =>
   configureStore({
     reducer: reduxSlices,
     devTools: false,
