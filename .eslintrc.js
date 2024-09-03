7a3e905069a298e8904d752d9a4ef9a5fa861ad5
“ <adam.morsi@yahoo.com>
2024-09-03 13:44:40 +0300

    update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -52,5 +52,6 @@ module.exports = {
     '@typescript-eslint/no-var-requires': 0,
     '@typescript-eslint/no-explicit-any': ['off'],
     '@typescript-eslint/no-unused-vars': ['warn'],
+    '@typescript-eslint/no-unused-expressions': ['off'],
   },
 };
